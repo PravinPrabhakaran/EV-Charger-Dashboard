2deb5f5221d3234cf4e25f407b6e1bbc783532e7
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2023-07-26 11:29:39 +0100

    improved quality image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import pegasusHouseImg from './PegasusHouseLayout.png';
 
 function App() {
   
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Works</h1>
+  <div class="building-layout">
+    <img class="pegasusClass" src={pegasusHouseImg} alt="Building Layout" />
+    <div class="overlay-container" id="rectangle1">
+      
     </div>
+    <div class="overlay-container" id="rectangle2"></div>
+ 
+  </div>
   );
 }
 
